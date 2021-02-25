2ac583dd3ab8f3cac6dfb63e3833d649b266d9de
oeltonsu88 <oscar.elton@datacivica.org>
2021-02-24 18:50:56 -0600

    test fuzzy join

--- import/src/src.R
+++ import/src/src.R
@@ -7,7 +7,7 @@
 
 # Paquetes ----------------------------------------------------------------
 if(!require('pacman')) {install.packages('pacman')}
-pacman::p_load(tidyverse, here, readxl, janitor, stringi)
+pacman::p_load(tidyverse, here, readxl, janitor, stringi, fuzzyjoin)
 
 
 
@@ -15,33 +15,34 @@ pacman::p_load(tidyverse, here, readxl, janitor, stringi)
 files <- list(registro_cbpcdmx = here("import", "input", "REGISTRO INTERNO CBP 1 febrero.xlsx"),
               registro_clean = here("import", "output", "registro_cdmx_clean"))
 
-
-
 clean_text <- function(s){
       str_squish(toupper(stri_trans_general(s, "latin-ascii")))
 }
 
-
 # Data ---------------------------------------------------------------------
-registro_cbpcdmx <- read_xlsx(files$registro_cbpcdmx) 
+registro_cbpcdmx <- read_xlsx(files$registro_cbpcdmx) %>% 
+   mutate(Colonia.de.la.desaparicion = clean_text(Colonia.de.la.desaparicion),
+          Municipio.Alcadia.de.la.desaparicion = clean_text(Municipio.Alcadia.de.la.desaparicion)) %>% 
+   rename(alcaldia = Municipio.Alcadia.de.la.desaparicion,
+          colonia = Colonia.de.la.desaparicion)
 
 colonias_cdmx <- read_csv("import/input/ColoniasCDMX_Oficial_Pobtot.csv") %>% 
       mutate(nombre = clean_text(nombre),
-             alcaldia = clean_text(alcaldia)) %>% 
+             alcaldia = clean_text(alcaldia),
+             cve_alc = str_pad(cve_alc, width = 3, side = "left", pad = "0")) %>% 
       rename(colonia = nombre)
 
 
-
-
-registro_cbpcdmx <- registro_cbpcdmx %>% 
-      mutate(Colonia.de.la.desaparicion = clean_text(Colonia.de.la.desaparicion),
-             Municipio.Alcadia.de.la.desaparicion = clean_text(Municipio.Alcadia.de.la.desaparicion)) %>% 
-      rename(mun_desp = Municipio.Alcadia.de.la.desaparicion,
-             colonia_desp = Colonia.de.la.desaparicion)
-
-
-registro_final <- left_join(registro_cbpcdmx, colonias_cdmx, 
-                            by = c(""))
+test_fuzzy <- registro_cbpcdmx %>% 
+   filter(!is.na(alcaldia) & !is.na(colonia)) %>% 
+   stringdist_left_join(colonias_cdmx, by = c("alcaldia", "colonia"), method = "lv",
+                        max_dist = 2, distance_col = "distance") %>% 
+   select(alcaldia.x, alcaldia.y, cve_alc, 
+          colonia.x, colonia.y, cve_col, 
+          alcaldia.distance, colonia.distance, everything())
+   
+   
+   
 
 
 
