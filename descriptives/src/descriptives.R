811f83e320ecf16739fd4c0448bca77d1221bc0a
Jorge Ruiz Reyes <ruiz_reyes_jorge@hotmail.com>
2021-07-11 20:06:18 -0500

    haciendo prueba de mapas

--- descriptives/src/descriptives.R
+++ descriptives/src/descriptives.R
@@ -8,12 +8,14 @@
 
 # Paquetes ----------------------------------------------------------------
 if(!require('pacman')) {install.packages('pacman')}
-pacman::p_load(tidyverse, sf, here, svglite, scales, treemapify, reshape2, rcolorbrewer)
+pacman::p_load(tidyverse, sf, here, svglite, scales, treemapify, reshape2, rcolorbrewer, readxl,  biscale, cowplot)
 
 
 
 # Files -------------------------------------------------------------------
 files <- list(registro_cbpcdmx_clean = here("import", "output", "registro_cdmx_clean.rds"),
+              evalua_cdmx = here("descriptives", "input", "coloniascdmx_ids1.xlsx"),
+              shp_cdmx = here("descriptives", "input", "coloniascdmx.shp"),
               perfiles_desp = here("descriptives", "output", "perfiles_desp."),
               perfiles_desp_estatus = here("descriptives", "output", "perfiles_desp_estatus."),
               tree_map_escolaridad = here("descriptives", "output", "escolaridad_desp."))
@@ -26,6 +28,15 @@ devices <- c("png", "svg")
 # Datos  ------------------------------------------------------------------
 
 registro_cbpcdmx_clean <- read_rds(files$registro_cbpcdmx_clean)
+
+evalua_cdmx <- read_excel(files$evalua_cdmx, skip = 1) %>% 
+   mutate(habitantes = as.numeric(habitantes)) %>% 
+   select(cve_col, habitantes, valor_ids, estrato)
+
+shp_cdmx <- st_read(files$shp_cdmx) %>% 
+   left_join(evalua_cdmx, 
+             by = c("cve_col" = "cve_col"))
+
 
 
 
@@ -152,6 +163,68 @@ registro_cbpcdmx_clean %>%
 
 walk(devices, ~ ggsave(filename = file.path(paste0(files$tree_map_escolaridad, .x)),
                        device = .x, width = 14, height = 10))
+
+
+
+# mapas desigualdad 
+
+cases_per_colonia <- registro_cbpcdmx_clean %>%
+   group_by(clave_colonia_desp) %>%
+   count(clave_municipio_desp) 
+
+
+
+# Datos para mapa
+mapa_desp_cdmx <- shp_cdmx %>%
+   left_join(cases_per_colonia,
+             by = c("cve_col" = "clave_colonia_desp")) %>% 
+   mutate(tasa_desp = n*(10000/habitantes),
+          valor_ids = as.numeric(valor_ids)) 
+   
+
+
+
+centroides <- st_coordinates(st_centroid(mapa_desp_cdmx)) %>% as.tibble()
+
+
+# create classes
+bi_map <- bi_class(mapa_desp_cdmx, x = valor_ids, y =  tasa_desp, style = "quantile", dim = 2) 
+
+bi_map <- bi_map %>% 
+   filter(bi_class != "1-NA") %>% 
+   filter(bi_class != "2-NA") %>% 
+   filter(bi_class != "NA-NA")
+
+# create map
+map <- ggplot() +
+   geom_sf(data = bi_map, mapping = aes(fill = bi_class), color = "grey", size = 0.2, show.legend = FALSE) +
+   bi_scale_fill(pal = "DkBlue", dim = 2) +
+   labs(
+      title = "Tasas de desaparición de personas en CDMX Y estrato social",
+      subtitle = "Nivel Colonia"
+   ) +
+   bi_theme() +
+   theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 12),
+         plot.subtitle = element_text(hjust = 0.5, face = "bold", size = 10),
+         plot.caption = element_text(hjust = 0.5, size = 10)) 
+
+map
+
+legend <- bi_legend(pal = "DkBlue",
+                    dim = 2,
+                    xlab = "Estrato Socioeconómico",
+                    ylab = "Tasa de desaparición por cada 10 mil habitantes",
+                    size = 7)
+
+
+finalPlot <- ggdraw() +
+   draw_plot(map, 0, 0, 1, 1) +
+   draw_plot(legend, 0.2, .65, 0.2, 0.2)
+
+finalPlot
+
+
+
 
 # FIN 
 
